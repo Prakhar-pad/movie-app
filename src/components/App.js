a7342f86efe1f285feb74e392c3ed3745ca92fac
Prakhar Agrawal <fzd.prakhar@gmail.com>
2021-06-28 15:36:44 +0530

    Navbar and moviecard added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {data} from '../data';
+import Navbar from './Navbar';
+import MovieCard from './MovieCard';
+import '../index.css';
 
 function App(){
     return(
         <div className='App'>
-            Project setup
+            <Navbar/>
+           <div className='tabs'>
+               <div className='tab'>Movies</div>
+               <div className='tab'>Favourites</div>
+            </div>
+
+            <div className='list'>
+                {data.map(movie => (
+                    <MovieCard movie={movie}/>
+                ))}
+            </div>
         </div>
     );
 }
