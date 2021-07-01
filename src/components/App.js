465f4215280f40b51d04c41a9c714e56552caa6e
Prakhar Agrawal <fzd.prakhar@gmail.com>
2021-07-01 19:25:19 +0530

    splitting reducers into movies and search reducers and added root reducer

--- src/components/App.js
+++ src/components/App.js
@@ -24,9 +24,9 @@ class App extends React.Component{
     }
 
     isMovieFavourite=(movie)=>{
-        const {favourites}=this.props.store.getState();
+        const {movies}=this.props.store.getState();
 
-        const index=favourites.indexOf(movie);
+        const index=movies.favourites.indexOf(movie);
 
         if(index!==-1){
             //found the movie
@@ -41,7 +41,9 @@ class App extends React.Component{
     }
 
     render(){
-        const { list, favourites, showFavourites }=this.props.store.getState();
+        const {movies}=this.props.store.getState();
+
+        const { list, favourites, showFavourites }=movies;
         console.log("RENDER");
         console.log('STATE', this.props.store.getState());
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,7 +6,7 @@ const initialMovieState={
     showFavourites: false
 }
 
-function movies(state=initialMovieState ,action){
+export function movies(state=initialMovieState ,action){
    switch(action.type){
        case ADD_MOVIES:
            return{
@@ -34,12 +34,28 @@ function movies(state=initialMovieState ,action){
                 ...state,
                 showFavourites:action.val
             }
-
-
-
         default:
             return state;
    }
 }
 
-export default movies;
+const initialSearchState={
+    result:{}
+};
+
+
+export function search(state=initialSearchState, action){
+    return state;
+}
+
+const initialRootState={
+    movies:initialMovieState,
+    search: initialSearchState
+}
+
+export default function rootReducer(state=initialRootState, action){
+    return {
+        movies: movies(state.movies, action),
+        search: search(state.search, action)
+    }
+}
